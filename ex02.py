94c8838794fb7e416536d0ecccd156ef9676fc9c
mrwog <mrwog308@gmail.com>
2025-08-13 19:20:55 -0700

    Module 1 commit

---
+++ ex02.py
@@ -0,0 +1,8 @@
+##############################
+#   ex02.py simple input     #
+# ############################
+
+#print('hello world')
+
+nzt = input('Enter your name: ')
+print('Hello', nzt)
---
+++ ex03.py
@@ -0,0 +1,10 @@
+##############################
+#   ex03.py gross pay calc   #
+# ############################
+
+#print('py4e')
+hours = input('Enter Hours: ').int(float)
+rate = input('Enter Rate: ').int(float)
+#pay = float(hours) * float(rate)
+pay = hours * rate
+print('Pay:',pay)
